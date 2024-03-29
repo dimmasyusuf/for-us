e9e7c006759ce15170a61e8cd80ec32452048151
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-29 20:41:36 +0700

    chore: setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { Button } from '@chakra-ui/react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from './app/store';
-import { decrement, increment } from './app/features/counterSlice';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
-    <div className="container vh-100 d-flex align-items-center justify-content-center gap-4">
-      <Button
-        colorScheme="blue"
-        onClick={() => dispatch(increment())}
-      >
-        +
-      </Button>
-      <span>{count}</span>
-      <Button
-        colorScheme="red"
-        onClick={() => dispatch(decrement())}
-      >
-        -
-      </Button>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <div className="container d-flex vh-100 align-items-center justify-content-center">
+      <Button colorScheme="teal">For Us</Button>
+    </div>
+  );
+}
